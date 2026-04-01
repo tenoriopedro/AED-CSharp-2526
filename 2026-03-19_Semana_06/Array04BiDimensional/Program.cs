cfdbe55d5dd1042d85a04670963760c3c22e7b92
Pedro Tenorio <pstsouza13@gmail.com>
2026-04-01 23:38:47 +0100

    style(week06): clean up formatting and restore execution blocks

--- 2026-03-19_Semana_06/Array04BiDimensional/Program.cs
+++ 2026-03-19_Semana_06/Array04BiDimensional/Program.cs
@@ -2,30 +2,30 @@
 // Teste de Arrays matriciais
 
 
-//int[,] matrix = new int[2, 3];    // 2 linhas e 3 colunas
+int[,] matrix = new int[2, 3];    // 2 linhas e 3 colunas
 
 
-//// Inserir dados numa matriz
-//for (int i = 0; i < matrix.GetLength(0); i++)
-//{
-//    for (int j = 0; j < matrix.GetLength(1); j++)
-//    {
-//        Console.Write($"Preenche a {i+1}º linha ; {j+1}º coluna: ");
-//        matrix[i, j] = Convert.ToInt32(Console.ReadLine());
-//    }
-//}
+// Inserir dados numa matriz
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"Preenche a {i + 1}º linha ; {j + 1}º coluna: ");
+        matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
 
-//Console.WriteLine("-----------------");
+Console.WriteLine("-----------------");
 
 
-//// Mostrar dados da Matriz
-//for (int i = 0;i < matrix.GetLength(0); i++)
-//{
-//    for(int j = 0;j < matrix.GetLength(1); j++)
-//    {
-//        Console.WriteLine($"Lista a {i+1}º linha ; {j+1}º coluna: {matrix[i, j]}");
-//    }
-//}
+// Mostrar dados da Matriz
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.WriteLine($"Lista a {i + 1}º linha ; {j + 1}º coluna: {matrix[i, j]}");
+    }
+}
 
 
 Console.WriteLine("-----------------");
@@ -35,11 +35,11 @@ int[,,] matrix3D = new int[2, 3, 2];
 
 
 // Inserir no Array 3D
-for(int i = 0; i < matrix3D.GetLength(0); i++)
+for (int i = 0; i < matrix3D.GetLength(0); i++)
 {
     for (int j = 0; j < matrix3D.GetLength(1); j++)
     {
-        for(int k = 0; k < matrix3D.GetLength(2); k++)
+        for (int k = 0; k < matrix3D.GetLength(2); k++)
         {
             Console.Write($"Preenche a {i + 1}º | {j + 1}º | {k + 1}º: ");
             matrix3D[i, j, k] = Convert.ToInt32(Console.ReadLine());
@@ -58,7 +58,7 @@ for (int i = 0; i < matrix3D.GetLength(0); i++)
         for (int k = 0; k < matrix3D.GetLength(2); k++)
         {
             Console.WriteLine($"        {matrix3D[i, j, k]}, ");
-            
+
         }
         Console.WriteLine("     }");
     }
